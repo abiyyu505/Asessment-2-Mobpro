3777c847b0e145dffb2abe53da54c0b74792fd96
abiyyu505 <abiyyumuhammad045@gmail.com>
2026-05-15 03:27:13 +0700

    Menampilkan list catatan

--- app/src/main/java/com/abiyyu0003/asessment2mobpro/ui/screen/MainScreen.kt
+++ app/src/main/java/com/abiyyu0003/asessment2mobpro/ui/screen/MainScreen.kt
@@ -1,12 +1,24 @@
 package com.abiyyu0003.mobpro1.ui.screen
 
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.abiyyu0003.mobpro1.model.Catatan
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -20,9 +32,57 @@ fun MainScreen() {
             )
         }
     ) { innerPadding ->
+        ScreenContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier) {
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
+    val context = LocalContext.current
+
+    if (data.isEmpty()) {
         Text(
-            text = "Aplikasi Catatan Materi Kuliah",
-            modifier = Modifier.padding(innerPadding)
+            text = "Belum ada catatan",
+            modifier = modifier.padding(16.dp)
+        )
+    } else {
+        LazyColumn(
+            modifier = modifier,
+            contentPadding = PaddingValues(bottom = 84.dp)
+        ) {
+            items(data) {
+                ListItem(catatan = it) {
+                    Toast.makeText(context, it.judul_materi, Toast.LENGTH_SHORT).show()
+                }
+                Divider()
+            }
+        }
+    }
+}
+
+@Composable
+fun ListItem(
+    catatan: Catatan,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .clickable { onClick() }
+            .padding(16.dp)
+    ) {
+        Text(
+            text = catatan.judul_materi,
+            style = MaterialTheme.typography.titleLarge
+        )
+        Text(
+            text = catatan.isi_materi,
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Text(
+            text = catatan.tanggal,
+            style = MaterialTheme.typography.bodySmall
         )
     }
 }
