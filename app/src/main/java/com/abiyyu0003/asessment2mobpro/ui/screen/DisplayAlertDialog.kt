4d52f171731fc4e3690e81d67692f3604abeba0d
abiyyu505 <abiyyumuhammad045@gmail.com>
2026-05-15 05:18:08 +0700

    Menambahkan dialog konfirmasi hapus

---
+++ app/src/main/java/com/abiyyu0003/asessment2mobpro/ui/screen/DisplayAlertDialog.kt
@@ -0,0 +1,31 @@
+package com.abiyyu0003.asessment2mobpro.ui.screen
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.abiyyu0003.asessment2mobpro.R
+
+@Composable
+fun DisplayAlertDialog(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit
+) {
+    AlertDialog(
+        text = {
+            Text(text = stringResource(R.string.hapus_pesan))
+        },
+        confirmButton = {
+            TextButton(onClick = onConfirmation) {
+                Text(text = stringResource(R.string.tombol_hapus))
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismissRequest) {
+                Text(text = stringResource(R.string.tombol_batal))
+            }
+        },
+        onDismissRequest = onDismissRequest
+    )
+}
